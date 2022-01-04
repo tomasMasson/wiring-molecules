a624a654c137c1b5e55b5f82c291ce561057ecb7
tmasson <tomasmasson0@gmail.com>
2022-01-05 00:47:11 +0100

    Added script to keep only the longest isoform of each gene in a proteome file (e.g. all CDS from D. melanogaster)

---
+++ workflow/scripts/filter_longest_isoform.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+"""
+Keeps the longest isoform of each coding sequence (CDS) in multifasta downloaded from FlyBase genome FTP.
+"""
+
+from typing import NewType
+from Bio import SeqIO
+import click
+
+
+def print_sequence_dict(seqs: dict) -> str:
+    """
+    Prints a multi sequence dictionary in fasta format.
+    """
+
+    # Iterate over the sequences dictionary
+    for seq in seqs:
+        sequence = seqs[seq]
+        # Print sequence in FASTA format
+        print(f">{sequence.description}\n{sequence.seq}")
+
+
+def get_identifier(seq: SeqIO.SeqRecord) -> str:
+    """
+    Parse the Flybase gene identifier from the fasta header. For this, it uses it the description attribute of SeqRecord object from Biopython SeqIO module.
+    In this particular case, the parser is prepared to deal with the fasta header implemented on genomic CDS multifasta files (e.g. http://ftp.flybase.net/genomes/Drosophila_willistoni/dwil_r1.3_FB2015_01/fasta).
+    """
+
+    gene_name = seq.description.split(';')[6][8:19]
+    return gene_name
+
+def compare_length(seq: SeqIO.SeqRecord) -> str:
+    """
+    """
+
+    length = len(seq) 
+
+    return length
+
+def get_longest_isoform(seqs: SeqIO.SeqRecord) -> str:
+    """
+    Print to standard output the longest isoform for each protein.
+    """
+
+    # Read initial sequences into a SeqIO.dict
+    seqs_dict = SeqIO.to_dict(SeqIO.parse(seqs, "fasta"))
+    # Initialize a dict to store the filteres sequences
+    seqs_filt = {}
+    # For each sequence in the dict
+    for key in seqs_dict:
+        # Retrieve the Seq object
+        seq = seqs_dict[key]
+        # Get sequence identifier
+        seq_id = get_identifier(seq)
+        # If the identifier is new in the filtered dict, just add the sequence
+        if seq_id not in seqs_filt.keys():
+            seqs_filt[seq_id] = seq
+        # The other option is that the identifier is already on the filtered dict.
+        # In that case, add the new sequence if it is longer than the existing one 
+        elif len(seq) > len(seqs_filt[seq_id]):
+            seqs_filt[seq_id] = seq
+    # Sent the filtered sequence dict into standard output
+    print_sequence_dict(seqs_filt)
+
+
+# Set CLI parameters
+## Add help page if no variable is provided through th command line
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("-s",
+              "--seqs",
+              "sequences",
+              help="Sequences to filtered")
+
+# Put all together into a principal function
+def main(sequences):
+    get_longest_isoform(sequences)
+
+
+if __name__ == "__main__":
+    main()
--- workflow/tmp/download_snippet.md
+++
@@ -1,3 +0,0 @@
-# Command line snippet to retrieve the protein sequences for a list of identifiers
-
-`for i in (cat id.csv); rsync -av rsync://ftp.ebi.ac.uk/ensemblgenomes/pub/release-51/metazoa/fasta/$i/pep/ datasets/$i; end`
