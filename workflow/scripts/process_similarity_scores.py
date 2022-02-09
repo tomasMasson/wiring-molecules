39f711812d17349c80134b590c052826ea33a4a8
tmasson <tomasmasson0@gmail.com>
2022-02-10 00:41:35 +0100

    Initial script to get the evolutionary correlation between genes from their phylogenetic profiles

---
+++ workflow/scripts/process_similarity_scores.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3.8
+
+"""
+Process the similarity scores from a Diamond blastp and returns a matrix with
+standarized scores
+"""
+
+import click
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def process_similarity_scores(matrix, output):
+    """
+    Transforms a tab-separated Diamond blastp results file into a matrix with
+    normalized values.
+    """
+
+    # Define columns names
+    columns = ["qseqid", "sseqid", "pident", "length",
+               "mismatch", "gapopen", "qstart", "qend",
+               "sstart", "send", "evalue", "bitscore"]
+    # Read data into a dataframe
+    df = pd.read_csv(matrix,
+                     sep="\t",
+                     names=columns,
+                     usecols=[0, 1, 11])
+    # Extract the species name from query identifiers
+    df["Species"] = df["qseqid"].str.split("_", expand=True).loc[:, 1]
+    # Pivot dataframe to separate different specie scores
+    df = df.pivot_table(index='sseqid', columns="Species", values="bitscore")
+    # Set query protein name as index
+    indexes = [index[2] for index in df.index.str.split("|")]
+    df.index = indexes
+    # Fill NaN values
+    df = df.fillna(20.4)
+    # Normalize blastp scores to Human reference values
+    gene_norm = df["HUMAN"]
+    df_n = df.divide(gene_norm, axis="index")
+    # Normalize for species divergence (Z-scaling across columns)
+    df_n = df_n.subtract(df_n.mean(axis=0), axis=1).divide(df_n.std(axis=0), axis=1)
+    # Save matrix to output file
+    df_n = df_n.drop('HUMAN', axis=1)
+    matrix = df.T.corr(method='pearson')
+    p = sns.heatmap(matrix, annot=True)
+    p.savefig("tmp.png")
+
+
+@click.command()
+@click.option("-i",
+              "--input",
+              help="Input file with Diamond blastp results.")
+@click.option("-o",
+              "--output",
+              help="Output file")
+def cli(input, output):
+    "Command line interface"
+    process_similarity_scores(input, output)
+
+
+if __name__ == "__main__":
+    cli()
