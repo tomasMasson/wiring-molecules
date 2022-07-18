4893cbb359012e63dea35c73579d841560973fb3
tmasson <tomasmasson0@gmail.com>
2022-07-19 01:05:32 +0200

    Filter_orthodb_tables.py now group orthologs and adds sequences counts

--- workflow/scripts/filter_orthodb_tables.py
+++ workflow/scripts/filter_orthodb_tables.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
 
-"Computes proteins members of each orthogroup at OrthoDB for a given taxon (e.g. Diptera)"
+"""
+Extract the members of each orthogroup stored at OrthoDB for a given taxon
+"""
 
 import click
 import pandas as pd
-from Bio import SeqIO
 
 
-def get_protein_names(multifasta, output):
+def filter_orthogroups_table(ogs, taxid):
     """
-    Creates a CSV file with the protein ID (Flybase) and its name
-    """
-
-    seqs = SeqIO.parse(multifasta, "fasta")
-    with open(output, "w") as fh:
-        for seq in seqs:
-            description = seq.description
-            protein_name = description.split(";")[3].split("name=")[1]
-            fh.write(f"{seq.id},{protein_name}\n")
-
-
-
-def filter_orthogroups(ogs, taxid):
-    """
-    Returns a filtered DataFrame from OrthoDB's orthogroups based on the TaxID
+    Returns a filtered DataFrame from OrthoDB orthogroups table (odb10v1_OGs)
+    based on the TaxID provided
     """
 
     # Column names
@@ -32,9 +20,8 @@ def filter_orthogroups(ogs, taxid):
     df = pd.read_csv(ogs,
                      sep="\t",
                      names=COLUMNS)
-    # Filter orthogroups according to the Taxid
+    # Filter orthogroups based on Taxid
     fdf = df[df["Level"] == taxid]
-    fdf.to_csv("diptera_orthogroups_intermediate.csv", index=False)
 
     return fdf
 
@@ -53,15 +40,15 @@ def map_ogs2genes(og2genes, ogs):
                                  sep="\t",
                                  names=COLUMNS,
                                  chunksize=1000000)
-    # Load orthogroups set
-    ogs_set = ogs["OG"]
+    # Load orthogroups list
+    ogs_list = ogs["OG"]
     # Iterate over genes table chunks
     for chunk in input_iterator:
-        # Filter for genes inside orthogroups set
-        chunk_flt = chunk[chunk["OG"].isin(ogs_set)]
+        # Keep genes present on orthogroups list
+        chunk_flt = chunk[chunk["OG"].isin(ogs_list)]
         # Add genes to output dataframe
         df = pd.concat([df, chunk_flt], ignore_index=True)
-    # Filter only orthogroups on the list
+    # Merge OG and OG2genes tables
     fdf = df.merge(ogs, on=["OG"])
 
     return fdf
@@ -69,10 +56,11 @@ def map_ogs2genes(og2genes, ogs):
 
 def add_gene_annotations(fgenes, annotations, species):
     """
-    Add annotations and species information to the gene/orthogroup dataframe
+    Add gene annotations (odb10v1_genes) and species (odb10v1_species)
+    information to the orthogroups dataframe
     """
 
-    # Read annotations table
+    # Load genes table
     COLUMNS = ["Gene_ID",
                "OrthoDB_species"]
     # Initialize DataFrame for gene annotations
@@ -106,40 +94,75 @@ def add_gene_annotations(fgenes, annotations, species):
     # Merge annotated genes and species DataFrames
     mdf = mdf.merge(species, on=["OrthoDB_species"])
     # Write final table to CSV
-    mdf.to_csv("diptera_orthogroups.csv", sep="\t")
+    mdf.to_csv("orthogroups_table.csv", sep="\t", index=False)
 
-    mdf_dmel = mdf[mdf["Scientific_name"] == "Drosophila melanogaster"]
-    genes_map = pd.read_csv("diamond_search_dmel_proteome", sep="\t", usecols=[0, 1], names=["OG", "Gene_name"])
-    mdf_dmel =mdf_dmel.set_index("Gene_ID").join(genes_map.set_index("Gene_ID"))
-    names = get_protein_names("dmel_proteome", "dmel_proteins_names")
-    mdf_dmel = pd.merge(mdf_dmel, names, on="Gene_name")
+    return mdf
+
+
+def group_genes_by_orthogroup(df, mappings, output):
+    """
+    Compute the number and list of proteins belonging to each orthogroup
+    """
+
+    # Load mappings between OrthoDB and Flybase identifiers
+    mappings = pd.read_csv(mappings,
+                           sep="\t",
+                           names=["orthodb", "flybase"])
+    maps = dict(zip(mappings.orthodb, mappings.flybase))
+    # Group genes by orthogroup
+    ogs = df.groupby("OG")
+    # dff = pd.DataFrame(columns=["Gene", "#_orthologs", "List_orthologs"])
+    data = []
+    COLUMNS = ["Gene_name", "#_orthologs", "List_orthologs"]
+    for og in ogs:
+        # Keep only orthogroups that contain D. melanogaster genes
+        species = list(og[1]["Scientific_name"])
+        if "Drosophila melanogaster" in species:
+            # Store species name
+            name = og[1][og[1].Scientific_name == "Drosophila melanogaster"].Gene_ID.values[0]
+            # Store gene name
+            genes = ",".join(list(og[1].Gene_ID))
+            # Store number of genes in the orthogroup
+            n_genes = len(og[1])
+            data.append([name, n_genes, genes])
+
+    dff = pd.DataFrame(data, columns=COLUMNS)
+    dff = dff.replace(maps)
+    dff.to_csv(output, sep="\t", index=False)
+    print(dff)
+
 
 # CLI options
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--orthogroups',
+@click.option('-o', '--orthogroups',
               help="Orthogroups table (e.g. 'odb10v1_OGs.tab')")
-@click.option('--taxid',
+@click.option('-t', '--taxid',
               type=int,
               help="Taxon identifier used to filter OrthoDB orthologs table")
-@click.option('--og2genes',
+@click.option('-og2g', '--og2genes',
               help="Mapping from orthogroups to gene names")
-@click.option('--genes',
+@click.option('-g', '--genes',
               help="Table with genes cross database information")
-@click.option('--species',
+@click.option('-s', '--species',
               help="Individual species information")
+@click.option('-m', '--mappings',
+              help="Mappigns from OrthoDB to Flybase identifiers")
+@click.option('-o', '--output',
+              help="File name for the output table")
 # CLI main function
-def command_line_interface(orthogroups, taxid, og2genes, genes, species):
+def cli(orthogroups, taxid, og2genes, genes, species, mappings, output):
     """
     Computes proteins members of each orthogroup at OrthoDB for a given taxon
     """
 
-    ogs = filter_orthogroups(orthogroups, taxid)
-    ogs_filtered = map_ogs2genes(og2genes, ogs)
-    add_gene_annotations(ogs_filtered, genes, species)
+    ogs = filter_orthogroups_table(orthogroups, taxid)
+    ogs_filt = map_ogs2genes(og2genes, ogs)
+    df = add_gene_annotations(ogs_filt, genes, species)
+    group_genes_by_orthogroup(df, mappings, output)
 
 
 if __name__ == '__main__':
-    command_line_interface()
+    cli()
